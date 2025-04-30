5dee2cb8a9f9531485ff16bc987d16c593d8c769
genaprhd <patternedgap@gmail.com>
2025-04-30 23:50:22 +0200

    Refactors main menu flow and removes async usage
    Simplifies menu logic by removing asynchronous patterns and ensures smoother recursive calls for menu navigation. Improves clarity in handling user actions and streamlines code for better maintainability.

--- DragonCave/Utilities/StartMenu.cs
+++ DragonCave/Utilities/StartMenu.cs
@@ -20,73 +20,76 @@ public class StartMenu
         Console.CursorVisible = false;
         UILib.ClearInputBuffer();
         Console.ReadKey(intercept: true);
-        _ =MainMenu();
+        MainMenu();
     }
-    static async Task MainMenu()
-    {   
+    static void MainMenu()
+    {
         Console.Clear();
         Console.CursorVisible = false;
         while (true)
         {
             selectedIndex = Menu.GetOption(MenuItems, selectedIndex, "Главное меню\n");
-                switch(selectedIndex){
-                     case 0:
-                        Character Player = PlayerProfileCreation.CreateProfile();
-                        Game.StartGame(Player);
-                        break;
-                    case 1:
-                        Console.Clear();
-                        UILib.TypeWriterEffect("Загрузка игры...");
-                        Console.Clear();
-                        if (JSONBase.IfAnyPlayerSaved()){
-                            int selectedIndex = Menu.GetOption(JSONBase.GetAllNames(), 0, "Выберите игрока:");
-                            if (Functions.InputConfirm($"Вы выбрали {JSONBase.GetAllNames()[selectedIndex]}. Подтвердите выбор?"))
-                                {
-                                    Player = JSONBase.LoadPlayer(selectedIndex);
-                                    UILib.TypeWriterEffect(Player.ToString());
-                                    UILib.TypeWriterEffect("Загрузка завершена.");
-                                    UILib.TypeWriterEffect("Нажмите любую клавишу, чтобы продолжить...");
-                                    Console.ReadKey(intercept: true);
-                                    Game.StartGame(Player);
-                                    break;
-                                }
-                                else
-                                {
-                                    UILib.TypeWriterEffect("Выбор отменен.");
-                                    Thread.Sleep(1000);
-                                    _ = MainMenu();
-                                };
+            switch (selectedIndex)
+            {
+                case 0:
+                    Character Player = PlayerProfileCreation.CreateProfile();
+                    Game.StartGame(Player);
+                    break;
+                case 1:
+                    Console.Clear();
+                    UILib.TypeWriterEffect("Загрузка игры...");
+                    Console.Clear();
+                    if (JSONBase.IfAnyPlayerSaved())
+                    {
+                        int selectedIndex = Menu.GetOption(JSONBase.GetAllNames(), 0, "Выберите игрока:");
+                        if (Functions.InputConfirm($"Вы выбрали {JSONBase.GetAllNames()[selectedIndex]}. Подтвердите выбор?"))
+                        {
+                            Player = JSONBase.LoadPlayer(selectedIndex);
+                            UILib.TypeWriterEffect(Player.ToString());
+                            UILib.TypeWriterEffect("Загрузка завершена.");
+                            UILib.TypeWriterEffect("Нажмите любую клавишу, чтобы продолжить...");
+                            Console.ReadKey(intercept: true);
+                            Game.StartGame(Player);
+                            break;
                         }
                         else
-                        {   
-                            UILib.TypeWriterEffect("Нет сохраненных игроков.");
-                            UILib.TypeWriterEffect("Нажмите любую клавишу, чтобы вернуться в меню...");
-                            Console.ReadKey(intercept: true);
-                            _ = MainMenu();
+                        {
+                            UILib.TypeWriterEffect("Выбор отменен.");
+                            Thread.Sleep(1000);
+                            MainMenu();
                         }
-                        break;
-                    case 2:
-                        Console.Clear();
-                        UILib.TypeWriterEffect("Кредиты:");
-                        UILib.TypeWriterEffect("Разработчик: Геннадий П.");
-                        UILib.TypeWriterEffect("Дизайнер: Геннадий П.");
-                        UILib.TypeWriterEffect("Тестировщик: Геннадий П.");
-                        UILib.TypeWriterEffect("Специальная благодарность: Геннадию П.");
-                        UILib.TypeWriterEffect("Версия: 1.0");
-                        UILib.TypeWriterEffect($"Дата выхода: {DateTime.Now.ToShortDateString()}");
-                        UILib.TypeWriterEffect("Лицензия: MIT");
-                        UILib.TypeWriterEffect("Год: 2025");
-                        UILib.TypeWriterEffect("Нажмите любую клавишу, чтобы вернуться в меню...");
-                        Console.CursorVisible = false;
-                        Console.ReadKey(intercept: true);
-                        _ = MainMenu();
-                        break;
+                        ;
                     }
-                    if (selectedIndex == MenuItems.Length - 1)
-                        ExitGame();
+                    else
+                    {
+                        UILib.TypeWriterEffect("Нет сохраненных игроков.");
+                        UILib.TypeWriterEffect("Нажмите любую клавишу, чтобы вернуться в меню...");
+                        Console.ReadKey(intercept: true);
+                        MainMenu();
+                    }
+                    break;
+                case 2:
+                    Console.Clear();
+                    UILib.TypeWriterEffect("Кредиты:");
+                    UILib.TypeWriterEffect("Разработчик: Геннадий П.");
+                    UILib.TypeWriterEffect("Дизайнер: Геннадий П.");
+                    UILib.TypeWriterEffect("Тестировщик: Геннадий П.");
+                    UILib.TypeWriterEffect("Специальная благодарность: Геннадию П.");
+                    UILib.TypeWriterEffect("Версия: 1.0");
+                    UILib.TypeWriterEffect($"Дата выхода: {DateTime.Now.ToShortDateString()}");
+                    UILib.TypeWriterEffect("Лицензия: MIT");
+                    UILib.TypeWriterEffect("Год: 2025");
+                    UILib.TypeWriterEffect("Нажмите любую клавишу, чтобы вернуться в меню...");
+                    Console.CursorVisible = false;
+                    Console.ReadKey(intercept: true);
+                    MainMenu();
                     break;
             }
+            if (selectedIndex == MenuItems.Length - 1)
+                ExitGame();
+            break;
         }
+    }
     public static void ExitGame()
     {
         Console.Clear();
